d3391d006d52b5c3a7bbf1789ce4b6e3728541c5
Leonardo Junior Formagini <leonardo.formagini@softplan.com.br>
2023-11-28 19:17:39 -0300

    componente introducao

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="contadores">
+        <h1>Contadores</h1>
+        <app-contador v-for="c in 5" :key="c" />
+        <button>?</button>
+    </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+export default {
+    components: {'app-contador': Contador}
+}
+</script>
+
+<style scoped>
+    div{
+        border: 1px solid red;
+    }
+</style>
