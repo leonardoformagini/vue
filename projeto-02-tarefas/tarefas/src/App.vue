8d70a875cb1f02443e81c0b76d7c98c6c3f2a087
Leonardo Formagini <leonardoformagini@gmail.com>
2023-12-14 11:47:34 -0300

    arquivos alterados

--- projeto-02-tarefas/tarefas/src/App.vue
+++ projeto-02-tarefas/tarefas/src/App.vue
@@ -1,14 +1,66 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
-		<input type="text" placeholder="Nova Tarefa?">
-		<button>+</button>
+		<TasksProgress :progress="progress"/>
+		<NewTask @taskAdded="addTask"/>
+		<TaskGrid 
+			@taskStateChanged="toggleTaskState"
+			@taskDeleted="deleteTask"
+			:tasks="tasks" />
+		
 	</div>
 </template>
 
 <script>
-export default {
+import NewTask from './components/NewTask.vue'
+import TaskGrid from './components/TaskGrid.vue'
+import TasksProgress from './components/TaskProgress.vue'
 
+export default {
+	components: { TaskGrid, NewTask, TasksProgress},
+	data(){
+		return{
+			tasks: []
+		}
+	},
+	computed: {
+		progress(){
+			const total = this.tasks.length
+			const done = this.tasks.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
+		}
+	},
+	watch:{
+		tasks: {
+			deep: true,
+			handler(){
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			}
+		}
+	},
+	methods: {
+		addTask(task){
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			if(reallyNew){
+				this.tasks.push({
+					name: task.name,
+					pending: task.pending || true
+				})
+			}
+		},
+		deleteTask(i){
+			this.tasks.splice(i,1)
+		},
+		toggleTaskState(i){
+			this.tasks[i].pending = !this.tasks[i].pending
+		}
+	},
+	created(){
+		const json = localStorage.getItem('tasks')
+		const array = JSON.parse(json)
+		this.tasks = Array.isArray(array) ? array : []
+	}
 }
 </script>
 
@@ -32,21 +84,5 @@ export default {
 		margin-bottom: 5px;
 		font-weight: 300;
 		font-size: 3rem;
-	}
-
-	input {
-		display: flex;
-		align-items: center;
-		font-family: 'Lato', sans-serif;
-		background: rgb(58, 96, 115);
-		color: #FFF;
-	}
-
-	button {
-		display: flex;
-		align-items: center;
-		font-family: 'Lato', sans-serif;
-		background: rgb(58, 96, 115);
-		color: #FFF;
 	}
 </style>
