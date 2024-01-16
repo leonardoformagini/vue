c558d97aef8fbd9f78c1356159592c6e5656defc
Leonardo Junior Formagini <leonardo.formagini@softplan.com.br>
2024-01-16 18:14:38 -0300

    aulas de http

---
+++ http/http-exercicios/src/plugins/axios.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+// axios.defaults.baseURL = 'https://curso-vue-d9976-default-rtdb.firebaseio.com'
+
+Vue.use({
+    install(Vue){
+        // Vue.prototype.$http = axios
+        Vue.prototype.$http = axios.create({
+            baseURL: 'https://curso-vue-d9976-default-rtdb.firebaseio.com',
+            headers: {
+                    "Authorization": "abc123"
+            }
+        })
+
+        Vue.prototype.$http.interceptors.request.use(config => {
+           
+            // if(config.method == 'post'){
+            //     config.method = 'put'
+            // }
+            // console.log(config.method)
+            return config
+        }, error => Promise.reject(error))
+
+        Vue.prototype.$http.interceptors.response.use(res => {
+            // const array = []
+            
+            // for(let chave in res.data){
+            //     array.push({id: chave, ...res.data[chave]})
+            // }
+
+            // res.data = array
+            return res
+        }, error => Promise.reject(error))
+    }
+
+    
+})
